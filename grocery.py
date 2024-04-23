6c4db6a9f8e3b6ed24aaebad7a7bd2b08f148f4d
Mausham Gurung <maushamgurung999@Maushams-Air.lan>
2024-04-23 19:02:13 +0100

    Adding a function to separate the shop items into two columns with price tag

--- grocery.py
+++ grocery.py
@@ -1,1 +1,24 @@
 from items import items
+
+class LocalStore:
+    def __init__(self):
+        self.items = items
+        self.cart = {}
+
+
+    def display_items(self):
+        """
+        Display store items into two separate columns with price rate
+        """
+        print("____Available Items____")
+        #Separate into two columns
+        half_len = len(self.items) // 2
+        #Left column
+        left_column = list(self.items.items())[:half_len]
+        #Right column
+        right_column = list(self.items.items())[half_len:]
+
+        for left_item, right_item in zip(left_column, right_column):
+            #Price tag for each item in the list
+            print(f"{left_item[0].capitalize()}: £{left_item[1]:.2f}\t\t{right_item[0].capitalize()}: £{right_item[1]:.2f}")
+
