2f6c1d7fc75ea96e5c510f86bc4427f1731472ff
Mausham Gurung <maushamgurung999@Maushams-Air.lan>
2024-04-23 19:21:01 +0100

    Adding a function where user can add the items from the list to their cart upto 100 each item

--- grocery.py
+++ grocery.py
@@ -21,4 +21,31 @@ class LocalStore:
         for left_item, right_item in zip(left_column, right_column):
             #Price tag for each item in the list
             print(f"{left_item[0].capitalize()}: £{left_item[1]:.2f}\t\t{right_item[0].capitalize()}: £{right_item[1]:.2f}")
+    
+
+    def add_to_cart(self, item):
+        """
+        Creats a cart for user to track the items added and able to selcet the quantity of items
+        """
+        if item.lower() in self.items:
+            #Can select the amount of quantity of items upto 100 per item
+            quantity = input(f"How many {item.capitalize()} do you want to add to your cart? (Limit 100): ")
+            if quantity.isdigit():
+                quantity = int(quantity)
+                if quantity <= 100:
+                    if item.lower() not in self.cart:
+                        self.cart[item.lower()] = quantity * self.items[items.lower()]
+                    else:
+                        self.cart[item.lower()] += quantity * self.items[item.lower()]
+                        print(f"{quantity} {item.capitalize()} added to your cart. ")
+                        self.display_cart()
+                else:
+                    print("You can only add up to 100 of each item.")
+            else:
+                print("Invalid quantity. Please enter correct number.")
+        else:
+            print("Invalid quantity. Please select from the list.")
+
+
+
 
